6199f1091eb409cd80f055554564759a5d020dfe
Anam Brazik <anam.brazik@gmail.com>
2021-08-09 21:22:26 -0400

    made tweaks to css, added empty js files for functions that need to be made, also moved html files to public folder.

---
+++ public/js/displayAppConf.js
@@ -0,0 +1,4 @@
+//TO DO:
+// display the appointment confirmation info from chatbot for the user 
+// Date, Time, and Service 
+// this function is for apptconf.html
--- public/js/displayMessages.js
+++ public/js/displayMessages.js
@@ -1,6 +1,7 @@
-
 // TO DO: 
 // .click on submit button
 // /fetch api
 // append text string from API
 // display in div
+
+// this function is for landingpage.html
