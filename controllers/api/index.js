f63a6d04c107caf5aa313d98bea2eff395a8ff91
jcorozco81 <jcorozco@gmail.com>
2021-08-11 00:51:21 -0400

    controllers structure

---
+++ controllers/api/index.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+// // const xxxRoutes = require('./xxxRoutes');
+// // const aaaRoutes = require('./aaaRoutes');
+
+// router.use('/xxx', xxxRoutes);
+// router.use('/aaa', aaaRoutes);
+
+module.exports = router;
