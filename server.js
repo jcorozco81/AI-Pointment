b6f75744949a1e836ea7d39c5bc1b71bb0791fa7
jcorozco81 <jcorozco@gmail.com>
2021-08-11 00:49:32 -0400

    Added handlebars and MVC structure

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 require('dotenv').config();
+
 const app = require('./app');
 const path = require('path');
+const routes = require('./controllers');
 
 
-
+// Sequelize
 const sequelize = require('./config/connection');
+
+// Load Handlebars
+const exphbs = require('express-handlebars');
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+
+app.use(routes);
+
 
 
 //START SERVER
