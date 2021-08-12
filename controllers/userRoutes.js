01ee311dc12bb4f428b237eb3db9cddaf471e699
jcorozco81 <jcorozco@gmail.com>
2021-08-12 19:14:33 -0400

    Initial commit user routes

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -1,5 +1,7 @@
+// All through http://localhost:4000/controller/user-routes
+
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User } = require('../models');
 
 router.post('/', async (req, res) => {
     try {
@@ -15,6 +17,52 @@ router.post('/', async (req, res) => {
       res.status(400).json(err);
     }
   });
+
+    // Get all users
+  router.get('/', async (req, res) => {
+    try {
+      const userData = await User.findAll();
+      res.status(200).json(userData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  // Get specific user by ID
+  router.get('/:id', async (req, res) => {
+    try {
+      const userData = await User.findByPk(req.params.id);
+      res.status(200).json(userData);
+if (!userData){
+    res.status(400).json({ message: 'User not found' });
+}
+
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+//   Modify user
+  router.put('/:id', async (req, res) => {
+    try {
+      const userData = await User.update(req.body,{
+        where: {
+            id: req.params.id,
+          },
+      });
+      res.status(200).json(userData);
+if (!userData){
+    res.status(400).json({ message: 'User not found' });
+}
+
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
+
+  
 
 
 
