e75136a4240297f203ec728fc3739463d4562ca5
jcorozco81 <jcorozco@gmail.com>
2021-08-12 21:19:21 -0400

    Fixed routes

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,8 @@
 const router = require('express').Router();
 const apiRoutes = require('./api/index');
 const homeRoutes = require('./homeRoutes');
-// const controllerRoutes = require('../controllers');
 
 router.use('/', homeRoutes);
-// router.use('/controller', controllerRoutes);
-
-// router.use('/api/v1');
-
 router.use('/api/v1', apiRoutes);
 
 module.exports = router;
