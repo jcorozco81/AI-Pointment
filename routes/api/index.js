99d31ceaffced40234f1bd81e71fddf045c57f68
jcorozco81 <jcorozco@gmail.com>
2021-08-13 00:45:14 -0400

    Fixed API routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,12 +3,15 @@ const dialogFlowRoutes = require('./dialogFlow');
 const authFlowRoutes = require('./auth');
 
 const userRoutes = require('./userRoutes');
-// const slotsRoutes = require('./slotsRoutes');
+const slotsRoutes = require('./slotsRoutes');
+const serviceRoutes = require('./serviceRoutes');
 
 router.use('/dialog-flow', dialogFlowRoutes);
 
+// db Routes
 router.use('/user', userRoutes);
-// router.use('/slots', slotsRoutes);
+router.use('/slots', slotsRoutes);
+router.use('/service', serviceRoutes);
 
 
 router.use('/auth', authFlowRoutes);
