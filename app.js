dfdf5be11ced05bb2b7fd7b5294024e076bff58c
Bernadette McKnight <sissyhanks@yahoo.com>
2021-08-14 15:31:47 -0400

    sessions save

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 const express = require('express');
+const routes = require('./routes/index');
+const Sequelize = require("sequelize");
 const session = require('express-session');
 
 const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const bcrypt = require('bcrypt');
-const catchAsync = require('./utils/catchAsync');
-const apiRoutes = require('./routes/index');
-
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const app = express();
+
+app.use(
+  session({
+    secret: "sign the cookie",
+    store: new SequelizeStore({
+      db: sequelize,
+    }),
+    resave: false, // we support the touch method so per the express-session docs this should be set to false
+    proxy: true, // if you do SSL outside of node.
+  })
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(`${__dirname}/public`));
 
 // the object each session receives
 const sess = {
   secret: 'key that will sign cookie',
-  cookie: { },
+  cookie: { maxAge: 7200000 },
   // we do not want a nes session each browser visit
   resave: false,
 //   don't save reoccurring visitors unless the session request is modified
@@ -26,17 +39,16 @@ const sess = {
 
 // innitialized middleware
 app.use(session(sess));
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static(`${__dirname}/public`));
+app.use(function (req, res, next) {
+    res.locals.session = req.session;
+    next();
+});
 
 app.use((req, res, next) => {
     req.requestTime = new Date().toISOString();
     next();
 });
 
-app.use('/', apiRoutes)
-
+app.use(routes);
 
 module.exports = app;
--- server.js
+++ server.js
@@ -3,11 +3,7 @@ require('dotenv').config();
 const app = require('./app');
 const path = require('path');
 
-
-
 const exphbs = require('express-handlebars');
-
-
 
 // Sequelize
 const sequelize = require('./config/connection');
@@ -19,17 +15,8 @@ const hbs = exphbs.create({});
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-
-
-
-
-
-
-
-
 //START SERVER
 const port = process.env.PORT || 4000;
-
 
 sequelize.sync({ force: false }).then(() => {
     app.listen(port, () => console.log(`App running on port ${port}...`));
