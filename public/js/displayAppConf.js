3931b866177febfc9f79ae187a51dd011df87ac8
Anam Brazik <anam.brazik@gmail.com>
2021-08-15 08:15:52 -0400

    messages should be displaying and appt conf should be working. deleted empty js files also.

--- public/js/displayAppConf.js
+++ public/js/displayAppConf.js
@@ -1,4 +1,31 @@
-//TO DO:
-// display the appointment confirmation info from chatbot for the user 
-// Date, Time, and Service 
-// this function is for apptconf.html
+$.urlParam = function(name){
+    var results = new RegExp('[\?&]' + name + '=([^&#]*)').exec(window.location.href);
+    if (results==null) {
+       return null;
+    }
+    return decodeURI(results[1]) || 0;
+}
+
+
+$(function() {
+
+    const slotsURL = `http://localhost:4000/api/v1/slots/${$.urlParam('id')}`;
+
+    fetch(slotsURL, {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then(data => data.json())
+        .then(data => {
+            console.log(data)
+            $('#confirmationNumber').append(`<h2><stropng>AI-Pointment Confirmation #:</strong> ${data.id}`)
+            $('#serviceMessage').append(`<h3><strong>Your service has been successfully scheduled for ${data.date}.</strong></h3>`)
+        })
+        .catch(function(err) {
+            console.log(err);
+        })
+        
+
+});
--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -35,6 +35,10 @@ router.get('/', async (req, res) => {
     res.render('signup');
   });
 
+  router.get('/confirmation', async (req, res) => {
+    res.render('confirmation');
+  });
+
   router.get('/login', async (req, res) => {
     res.render('login');
   });
@@ -51,4 +55,61 @@ router.get('/message-test', async (req, res) => {
 
 
 module.exports = router;
+
+
+
+
+// const router = require('express').Router();
+// // const userControllerRoute = require('../controllers/user');
+// const { User } = require('../models');
+
+
+// router.get('/', async (req, res) => {
+//   res.render('landing');
+// });
+
+
+//   router.get('/profile', async (req, res) => {
+//     try {
+//       // Find the logged in user based on the session ID
+//       const userData = await User.findByPk(1);
+  
+//       const user = userData.get({ plain: true });
+//       console.log(user);
+  
+//       res.render('profile', {
+//         ...user});
+//     } catch (err) {
+//       res.status(500).json(err);
+//     }
+//   });
+
+
+
+
+
+
+
+
+
+//   router.get('/signup', async (req, res) => {
+//     res.render('signup');
+//   });
+
+//   router.get('/login', async (req, res) => {
+//     res.render('login');
+//   });
+
+
+
+
+
+  
+
+// router.get('/message-test', async (req, res) => {
+//   res.render('messageTest');
+// });
+
+
+// module.exports = router;
 
