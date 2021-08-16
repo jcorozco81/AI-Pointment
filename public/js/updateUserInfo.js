8221bb8313c13a23b2b75110eb50423461b8b899
Ethan Harsh <ethan@ethanharsh.com>
2021-08-16 06:49:34 +0000

    profile javascript

--- public/js/updateUserInfo.js
+++ public/js/updateUserInfo.js
@@ -3,51 +3,71 @@ console.log("loaded signup");
 const updateUserInfo = async (event) => {
   event.preventDefault();
   const id = event.target.getAttribute('user-id');
-  console.log("You clicked me!");
-  
+  //console.log("You clicked me!");
 
-  const first_name = document.querySelector('#first-name-update').value.trim();
-  console.log(first_name);
 
-  const last_name = document.querySelector('#last-name-update').value.trim();
-  console.log(last_name);
+  const first_name = document.querySelector('#first_name').value.trim();
+  //console.log(first_name);
 
-  const phone = document.querySelector('#phone-update').value.trim();
-  console.log(phone);
+  const last_name = document.querySelector('#last_name').value.trim();
+  //console.log(last_name);
 
-  const address = document.querySelector('#address-update').value.trim();
-  console.log(address);
+  const phone = document.querySelector('#phone').value.trim();
+  //console.log(phone);
 
-  const car_make = document.querySelector('#make-update').value.trim();
-  console.log(car_make);
+  const address = document.querySelector('#address').value.trim();
+  //console.log(address);
 
-  const car_model = document.querySelector('#model-update').value.trim();
-  console.log(car_model);
+  const response = await fetch(`/api/v1/user/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({ first_name, last_name, phone, address }),
+    headers: { 'Content-Type': 'application/json' },
+  });
 
-  const year = document.querySelector('#year-update').value.trim();
+  if (response.ok) {
+    //   document.location.replace('/profile');
+  } else {
+    alert("Test" + response.statusText);
+  }
+}
+
+const updateCarInfo = async (event) => {
+  event.preventDefault();
+  const id = event.target.getAttribute('user-id');
+
+  const car_make = document.querySelector('#make').value.trim();
+  //console.log(car_make);
+
+  const car_model = document.querySelector('#model').value.trim();
+  //console.log(car_model);
+
+  const year = document.querySelector('#year').value.trim();
   const car_year = parseInt(year);
-  console.log(car_year);
+  //console.log(car_year);
 
   // (first_name && last_name && phone && address && car_make && car_model && car_year) {
-      console.log(id);
-    
-    const response = await fetch(`/api/v1/user/${id}`, {
-      method: 'PUT',
-      body: JSON.stringify({ first_name, last_name, phone, address, car_make, car_model, car_year }),
-      headers: { 'Content-Type': 'application/json' },
-    });
+  console.log(id);
 
-    if (response.ok) {
+  const response = await fetch(`/api/v1/user/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({ first_name, last_name, phone, address, car_make, car_model, car_year }),
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
     //   document.location.replace('/profile');
-    } else {
-      alert("Test"+response.statusText);
-    }
+  } else {
+    alert("Test" + response.statusText);
   }
-// };
+}
 
-window.onload = function(){
-console.log("loaded function signup");
+window.onload = function () {
+  console.log("loaded function signup");
   document
-  .querySelector("#update-info")
-  .addEventListener("click", updateUserInfo);
+    .querySelector("#update")
+    .addEventListener("click", updateUserInfo);
+
+  document
+    .querySelector("#update-car")
+    .addEventListener("click", updateCarInfo);
 }
